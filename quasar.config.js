16fae6a3e0b8e399ea9a7172a681b3f93aade10e
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2026-06-11 12:28:56 +0300

    chore: update to latest

--- quasar.config.js
+++ quasar.config.js
@@ -115,7 +115,7 @@ export default defineConfig((/* ctx */) => {
     //   router: 'src/router/index',
     //   store: 'src/store/index',
     //   pwaRegisterServiceWorker: 'src-pwa/register-sw',
-    //   pwaServiceWorker: 'src-pwa/custom-sw',
+    //   pwaServiceWorker: 'src-pwa/sw/custom-sw',
     //   pwaManifestFile: 'src-pwa/manifest.json',
     //   electronMain: 'src-electron/electron-main',
     //   electronPreload: 'src-electron/electron-preload'
